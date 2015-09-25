ea2ee47a63dfd27bab3491fda34ac410711f0af9
Adam <adam.drake@corusent.com>
2015-09-25 12:22:03 -0400

    Some semi-final touches. I have to test this later but this is 80% ready to go.

--- roles/adamd-test-box.rb
+++ roles/adamd-test-box.rb
@@ -1,5 +1,11 @@
+# https://adamcod.es/2013/01/15/vagrant-is-easy-chef-is-hard-part2.html
 # Name of the role should match the name of the file
 name "adamd-test-box"
+default_attributes(
+    "build_essential" => {
+        "compiletime" => true
+    }
+)
 # MySQL db connection basics (for setup only)
 override_attributes(
     "mysql" => {
@@ -10,6 +16,7 @@ override_attributes(
 )
 # Recipes to run, in the correct sequence
 run_list(
+    "recipe[build-essential]",
     "recipe[openssl]",
     "recipe[apache2]",
     "recipe[apache2::mod_php5]",
@@ -17,5 +24,7 @@ run_list(
     "recipe[mysql::server]",
     "recipe[php]",
     "recipe[php::module_mysql]",
-    "recipe[apache2::vhosts]"
+    "recipe[apache2::vhosts]",
+    "recipe[database::mysql]",
+    "recipe[database::import]"
 )
